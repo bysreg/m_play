445cabb1d674d58537a001c410251eadbf0dcdb3
hilman <hilmanbeyri@gmail.com>
2016-01-20 12:53:56 -0500

    Add index.html template view and add three.min.js

--- src/m_play/gnovel/views.py
+++ src/m_play/gnovel/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-	return HttpResponse("Hello world!")
+	context = {
+		'text': 'sing ngarso sung tulodo'
+	}
+	return render(request, 'gnovel/index.html', context)
