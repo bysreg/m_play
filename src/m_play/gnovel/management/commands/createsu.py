9e0bf34aedb689331d216d6f8eb7f710d908f6ef
hilman <hilmanbeyri@gmail.com>
2016-01-21 18:51:11 -0500

    Change password for admin in createsu.py

--- src/m_play/gnovel/management/commands/createsu.py
+++ src/m_play/gnovel/management/commands/createsu.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "moralityplaycmu@gmail.com", "morallarom")
+            User.objects.create_superuser("admin", "moralityplaycmu@gmail.com", "moralpitt")
