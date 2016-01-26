13bcefacc0ab58953ef0f6cd2c2a8305b21403ba
hilman <hilmanbeyri@gmail.com>
2016-01-26 18:02:32 -0500

    Fix typos for path in setup.sh and m_play_nginx.conf

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 python3 ../src/m_play/manage.py createsu
 python3 ../src/m_play/manage.py migrate
 
-echo "for production, symlink to /src/m_play/m_play_nginx.conf from /etc/nginx/sites-enabled"
-sudo ln -s ~/path/to/your/mysite/mysite_nginx.conf /etc/nginx/sites-enabled/
+echo "for production, symlink to ../src/m_play/m_play_nginx.conf from /etc/nginx/sites-enabled"
+sudo ln -s ../src/m_play/m_play_nginx.conf /etc/nginx/sites-enabled/
