1ab50a549a6afb43b84101a71c5aceff7e4daeaa
hilman <hilmanbeyri@gmail.com>
2016-01-26 18:25:00 -0500

    Fix symbolic link path for nginx conf in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,4 +2,5 @@ python3 ../src/m_play/manage.py createsu
 python3 ../src/m_play/manage.py migrate
 
 echo "for production, symlink to ../src/m_play/m_play_nginx.conf from /etc/nginx/sites-enabled"
-sudo ln -s ../src/m_play/m_play_nginx.conf /etc/nginx/sites-enabled/
+cwd=$(pwd)
+sudo ln -s $(pwd)/../src/m_play/m_play_nginx.conf /etc/nginx/sites-enabled/
