d0e0aad04e2aff1475bfee26ca28c661c720a44d
hilman <hilmanbeyri@gmail.com>
2016-01-26 17:49:42 -0500

    change localhost db name and username

--- src/m_play/m_play/settings.py
+++ src/m_play/m_play/settings.py
@@ -119,8 +119,8 @@ else:
 	DATABASES = {
 		'default': {
 			'ENGINE': 'django.db.backends.postgresql_psycopg2',
-			'NAME': 'm_play',
-			'USER' : 'm_play',
+			'NAME': 'm_play_test',
+			'USER' : 'm_play_test',
 			'PASSWORD' : 'test', 
 			'HOST' : '127.0.0.1',
 			'PORT' : '5432',
