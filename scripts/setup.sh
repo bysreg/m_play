e71986377d720c8cc90842e3ea48ac8b38ed5ad3
hilman <hilmanbeyri@gmail.com>
2016-01-26 17:24:17 -0500

    Fix typo for manage.py file in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 python3 ../src/m_play/manage.py createsu
-python3 ../src/m_play/mmanage.py migrate
+python3 ../src/m_play/manage.py migrate
 
 echo "for production, symlink to /src/m_play/m_play_nginx.conf from /etc/nginx/sites-enabled"
 sudo ln -s ~/path/to/your/mysite/mysite_nginx.conf /etc/nginx/sites-enabled/
