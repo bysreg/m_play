e935eecc6aa02e80b9b739f626e8ed18cfabf5f1
hilman <hilmanbeyri@gmail.com>
2016-01-26 19:10:19 -0500

    Modify script to run production server

--- scripts/run_production.sh
+++ scripts/run_production.sh
@@ -1,3 +1,5 @@
 mkdir -p ../../static
 python3 ../src/m_play/manage.py collectstatic
-python3 ../src/m_play/manage.py runserver
+sudo /etc/init.d/nginx restart
+cd ../src/m_play/
+uwsgi --socket :8001 --module m_play.wsgi:application --chmod-socket=664
