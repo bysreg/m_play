c2d3cb729d57b93b1c62c0ed9ba14d1ee1001686
hilman <hilmanbeyri@gmail.com>
2016-01-27 19:19:40 -0500

    Change script to use .ini file

--- scripts/run_production.sh
+++ scripts/run_production.sh
@@ -2,4 +2,4 @@ mkdir -p ../../static
 python3 ../src/m_play/manage.py collectstatic
 sudo /etc/init.d/nginx restart
 cd ../src/m_play/
-uwsgi --socket :8001 --module m_play.wsgi:application --chmod-socket=664
+uwsgi --ini m_play_uwsgi.ini
