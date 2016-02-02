bf1da8bcef9cd37f62299190aff19ff43efd09a7
hilman <hilmanbeyri@gmail.com>
2016-02-01 20:53:57 -0500

    Override onMouseDown in Page1

--- src/m_play/gnovel/static/gnovel/js/mplay/page1.js
+++ src/m_play/gnovel/static/gnovel/js/mplay/page1.js
@@ -1,4 +1,3 @@
-
 // namespace
 var MPLAY = MPLAY || {};
 
@@ -17,10 +16,20 @@ var MPLAY = MPLAY || {};
 	Page1.prototype = Object.create(GNOVEL.Page.prototype);
 	Page1.prototype.constructor = Page1;
 
+	/**
+	 * @override	 
+	 */
 	Page1.prototype._onLoad = function() {
 		GNOVEL.Page.prototype._onLoad.call(this);
 
 		this.setBackground("/static/gnovel/res/textures/test1.jpg");
+	};
+
+	/**
+	 * @override	 
+	 */
+	Page1.prototype._onMouseDown = function(event) {
+		this._owner.goToPage(1, GNOVEL.TransitionType.FADE);
 	};
 
 	MPLAY.Page1 = Page1;
