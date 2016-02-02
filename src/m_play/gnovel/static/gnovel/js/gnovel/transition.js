62e9554edac5414d95432a11a5070668aeb642bf
hilman <hilmanbeyri@gmail.com>
2016-02-01 20:54:53 -0500

    Add class Page2 & Transition and modify index.html

---
+++ src/m_play/gnovel/static/gnovel/js/gnovel/transition.js
@@ -0,0 +1,51 @@
+// namespace :
+var GNOVEL = GNOVEL || {};
+
+(function() {
+	"use strict";
+
+	/**
+	 *transition to next scene over certain time
+	 * @class Transition
+	 * @constructor
+	 * @param {time} time for transition to play, in milliseconds
+	 **/
+	var Transition = function(time) {
+		this.time = time || 400;
+	};
+
+	/**
+	 * Do Transition. only called by the Manager
+	 * @method run
+	 * @param {currentPage} currentPage, the current page to be tansitioned from
+	 * @param {nextPage} the new page to show
+	 **/
+	Transition.prototype.run = function(currentPage, nextPage) {
+		var duration = this.time;
+
+		currentPage.getBackground().material.opacity = 0;
+		nextPage.getBackground().material.opacity = 0;
+
+		///*
+		//tween opacity for fade over duration time.
+		var tween_out = new TWEEN.Tween(currentPage.getBackground().material)
+			.to({
+				opacity: 0
+			}, duration)
+			.start();
+
+		var tween_in = new TWEEN.Tween(nextPage.getBackground().material)
+			.to({
+				opacity: 1
+			}, duration)
+			.start();
+		//*/
+	};
+	
+	// transition type
+	GNOVEL.TransitionType = {};
+	GNOVEL.TransitionType.FADE = 0;
+
+	GNOVEL.Transition = Transition;
+
+}());
---
+++ src/m_play/gnovel/static/gnovel/js/mplay/page2.js
@@ -0,0 +1,30 @@
+// namespace
+var MPLAY = MPLAY || {};
+
+(function() {
+	"use_strict";
+
+	/**
+	 *
+	 * @class Page2
+	 * @augments GNOVEL.Page
+	 */
+	var Page2 = function() {
+		GNOVEL.Page.call(this);
+	};
+
+	Page2.prototype = Object.create(GNOVEL.Page.prototype);
+	Page2.prototype.constructor = Page2;
+
+	/**
+	 * @override	 
+	 */
+	Page2.prototype._onLoad = function() {
+		GNOVEL.Page.prototype._onLoad.call(this);
+
+		this.setBackground("/static/gnovel/res/textures/steven_universeXworlds8.jpg");		
+		this._bg.position.z = 90;
+	};
+
+	MPLAY.Page2 = Page2;
+}());
