c4646cdb587ada8836ca7ccd84feb8fcfa627217
hilman <hilmanbeyri@gmail.com>
2016-02-15 12:54:26 -0500

    Add multiple Flow based on which button clicked

--- src/m_play/gnovel/static/gnovel/js/mplay/page0.js
+++ src/m_play/gnovel/static/gnovel/js/mplay/page0.js
@@ -30,8 +30,24 @@ var MPLAY = MPLAY || {};
 		this._professorImg = this.createImage("/static/gnovel/res/textures/char/prof sweeney- thoughtful.png", new THREE.Vector3(75, -130, 180), 600, 750);
 		this._juliImg = this.createImage("/static/gnovel/res/textures/char/thoughtful-julia.png", new THREE.Vector3(-300, -140, 120), 600, 750);
 		this._ryanImg = this.createImage("/static/gnovel/res/textures/char/ryan-happy.png", new THREE.Vector3(0, -80, 140), 600, 750);
-		this._catImg = this.createImage("/static/gnovel/res/textures/char/cat-annoyed.png", new THREE.Vector3(450, -130, 100), 600, 750);
-		this._button = this.createImage("/static/gnovel/res/textures/Perspective-Button-Stop-icon.png", new THREE.Vector3(0, 0, 0), 256, 256);
+		this._catImg = this.createImage("/static/gnovel/res/textures/char/cat-annoyed.png", new THREE.Vector3(450, -130, 100), 600, 750);		
+
+		this._talked = 0;
+
+		var pageObj = this;
+		var io1 = this.createInteractableObject(
+			"/static/gnovel/res/textures/Perspective-Button-Stop-icon.png", 
+			{x: -100, y: 200, width : 64, height : 64, onClick: function() {
+				pageObj._talked = 1;
+				pageObj._runFlow();
+			}});		
+
+		var io2 = this.createInteractableObject(
+			"/static/gnovel/res/textures/Perspective-Button-Stop-icon.png", 
+			{x: 100, y: 200, width : 64, height : 64, onClick: function() {
+				pageObj._talked = 2;
+				pageObj._runFlow();
+			}});		
 
 		this._professorImg.material.opacity = 0;
 		this._juliImg.material.opacity = 0;
@@ -55,20 +71,28 @@ var MPLAY = MPLAY || {};
 		var juli = "%juli";
 		var ryan = "%ryan";
 		var cat = "%cat";
+		var o = null;
 
-		var o = [
-			{type: "dialog", text: "... And as we wrap up today's class. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu ornare enim. Praesent lectus diam, ornare iaculis purus aliquam, euismod mollis odio. Integer sed congue sapien. Vestibulum eget molestie lorem"}, 
-			{type: "choices", choices : [{text: "Not at all!", go: "#choice_number_1"}, {text : " Oh, what did you do before deciding to get your MBA?", go : "#choice_number_2"}]},
-			{type: "dialog", text: "you picked choice number 1", label : "choice_number_1"}, 
-			{type: "dialog", text: "you picked choice number 2", label : "choice_number_2"},			
-			{type: "show", img: professor},			
-			{type: "show", img: cat},
-			{type: "hide", img: professor, waitUntilHidden: false},
-			{type: "show", img: ryan}, 
-			//{type: "hide", img: ryan},
-			//{type: "hide", img: cat},
-			{type: "goto", page: 1, transition: "fade"},
-		];
+		if(this._talked == 1) {
+			o = [
+				{type: "dialog", text: "... And as we wrap up today's class. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu ornare enim. Praesent lectus diam, ornare iaculis purus aliquam, euismod mollis odio. Integer sed congue sapien. Vestibulum eget molestie lorem"}, 
+				{type: "choices", choices : [{text: "Not at all!", go: "#choice_number_1"}, {text : " Oh, what did you do before deciding to get your MBA?", go : "#choice_number_2"}]},
+				{type: "dialog", text: "you picked choice number 1", label : "choice_number_1"}, 
+				{type: "dialog", text: "you picked choice number 2", label : "choice_number_2"},			
+				{type: "show", img: professor},			
+				{type: "show", img: cat},
+				{type: "hide", img: professor, waitUntilHidden: false},
+				{type: "show", img: ryan}, 
+				{type: "hide", img: ryan},
+				{type: "hide", img: cat},
+				//{type: "goto", page: 1, transition: "fade"},
+			];
+		}		
+		else if(this._talked == 2) {
+			o = [
+				{type: "dialog", text: "dialog with character number 2. bla bla"}, 
+			];
+		}
 		
 		return o;
 	};
