80dae706565db2ca255df22d469703d5f5d18057
hilman <hilmanbeyri@gmail.com>
2016-02-15 12:50:36 -0500

    Add class InteractableObject

---
+++ src/m_play/gnovel/static/gnovel/js/gnovel/interactableobject.js
@@ -0,0 +1,60 @@
+// namespace 
+var GNOVEL = GNOVEL || {};
+
+(function() {
+	"use strict";
+
+	/**
+	 *
+	 * @class InteractableObject
+	 * @constructor
+	 * 
+	 */
+	var InteractableObject = function(path, page, params) {
+		params = params || {};
+		this._page = page;
+		this._mouseDownListener = null;
+		this._img = null;
+		this._params = params;
+
+		// create the image
+		// for now we require width and height in params
+		var x = params.x || 0;
+		var y = params.y || 0;
+		var pos = new THREE.Vector3(x, y, 100);
+		this._img = this._page.createImage(path, pos, this._params.width, this._params.height);
+
+		this._page._addToScene(this._img);
+
+		var io = this;
+		this._mouseDownListener = function(event) {			
+			io._onMouseDown(event);
+		};
+		document.addEventListener('mousedown', this._mouseDownListener, false);
+	};
+
+	InteractableObject.prototype._onMouseDown = function(event) {
+		var mouse = new THREE.Vector2();
+
+		event.preventDefault();
+
+		mouse.x = (event.clientX / this._page._owner._renderer.domElement.clientWidth) * 2 - 1;
+		mouse.y = -(event.clientY / this._page._owner._renderer.domElement.clientHeight) * 2 + 1;
+
+		this._page._owner._raycaster.setFromCamera(mouse, this._page._owner.getCamera());
+		//create array of objects intersected with
+		var intersects = this._page._owner._raycaster.intersectObjects([this._img], true);
+		if (intersects.length > 0) {
+			var clickedObj = intersects[0].object;
+
+			console.log("interactable object is clicked");
+
+			if(this._params.onClick != null) {
+				this._params.onClick(this);
+			}
+		}
+	};
+
+	GNOVEL.InteractableObject = InteractableObject;
+}());
+
