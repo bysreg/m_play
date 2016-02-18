9aac2c5a9e5325658a53f7cd316bb906290552f8
narumimaria <zhangyaz@andrew.cmu.edu>
2016-02-18 18:17:21 -0500

    added waitUntilShown to scene 2

--- src/m_play/gnovel/static/gnovel/js/mplay/page1.js
+++ src/m_play/gnovel/static/gnovel/js/mplay/page1.js
@@ -78,42 +78,42 @@ var MPLAY = MPLAY || {};
 
 		if(this._talked == 0) {
 			o = [
-				{type: "show", img: professor, position: "center"},
+				{type: "show", img: professor, position: "center", waitUntilShown: false},
 				{type: "dialog", speaker: "professor", text: "My aim in Programming and Society is to help you become a better programmer. Technology impacts how society operates. I will challenge you to look deeper into the programs, apps, and technologies that you use."},
 				{type: "dialog", speaker: "professor", text: "The second half of each lesson will break into your assigned study groups. Please use this time wisely, you’ll have a group project later in the semester."}, 
 				{type: "hide", img: professor, waitUntilHidden: false},
-				{type: "show", img: ryan, position: "center"},
+				{type: "show", img: ryan, position: "center", waitUntilShown: false},
 				{type: "dialog", speaker: "ryan", text: "Awesome! Both you and Priya are in my group. This is Priya. She’s super smart, speaks like a bajillion languages. Too cool for me."},
 				{type: "hide", img: ryan, waitUntilHidden: false},
-				{type: "show", img: priya, position: "left"},
+				{type: "show", img: priya, position: "left", waitUntilShown: false},
 				{type: "dialog", speaker: "priya", text: "RYAN here helped me with some of my writing last semester. Always willing to be my study partner. I think I annoyed him most of the time. Anyway, nice to meet you"},
 				{type: "hide", img: priya, waitUntilHidden: false},
-				{type: "show", img: cat, position: "right"},
+				{type: "show", img: cat, position: "right", waitUntilShown: false},
 				{type: "dialog", speaker: "cat", text: "Hey I’m Cat. I’m in Tepper. I think I’m going to be in over my head a little here in a CS class It’s been an adjustment coming back to school."},
 				{type: "hide", img: cat, waitUntilHidden: false},
-				{type: "show", img: ryan, position: "center"},
+				{type: "show", img: ryan, position: "center", waitUntilShown: false},
 				{type: "dialog", speaker: "ryan", text: "Oh, don’t sweat it, Cat. We’ll help you out if you get stuck."},
 				{type: "hide", img: ryan, waitUntilHidden: false},
-				{type: "show", img: cat},
+				{type: "show", img: cat, waitUntilShown: false},
 				{type: "dialog", speaker: "cat", text: "Sorry - I’m a little all over the place. I lost my phone yesterday."},
 				{type: "hide", img: cat, waitUntilHidden: false},
 				{type: "compare", leftop: isPhonePickedUp, operator: "equal", rightop: 1, goTrue: "#phone_picked", goFalse: "#phone_notpicked"},
-				{type: "show", img: ryan, label: "phone_picked"},
+				{type: "show", img: ryan, label: "phone_picked", waitUntilShown: false},
 				{type: "dialog", speaker: "ryan", text: "Were you at Scottie’s Bar yesterday? " + player +" found a phone there."},
 				{type: "hide", img: ryan, waitUntilHidden: false},
-				{type: "show", img: cat},
+				{type: "show", img: cat, waitUntilShown: false},
 				{type: "dialog", speaker: "cat", text: "Oh my God, do you guys have it with you?"},
 				{type: "hide", img: cat, waitUntilHidden: false},
-				{type: "show", img: ryan},
+				{type: "show", img: ryan, waitUntilShown: false},
 				{type: "compare", leftop: isPhoneWithYou, operator: "equal", rightop: 1, goTrue: "#phone_withyou", goFalse: "#phone_notwithyou"},
 				{type: "dialog", speaker: "ryan", text: "Yeah, " + player + " has it, right?", label: "phone_withyou"},
 				{type: "hide", img: ryan, waitUntilHidden: false},
-				{type: "show", img: cat},
+				{type: "show", img: cat, waitUntilShown: false},
 				{type: "dialog", speaker: "cat", text: "Oh my God - you charged it too? You’ve totally restored my faith in humanity."},
 				{type: "jump", condition: true, goTrue: "#choices", goFalse: 1000},
 				{type: "dialog", speaker: "ryan", text: "We left it with the bartender.", label: "phone_notwithyou"},
 				{type: "hide", img: ryan, waitUntilHidden: false},
-				{type: "show", img: cat},
+				{type: "show", img: cat, waitUntilShown: false},
 				{type: "dialog", speaker: "cat", text: "Thank you! I’ll run over there after this. Lifesaver!"},
 				{type: "choices", choices : [{text: "No Problem.", go: "#hidecat", relationship: {name:"cat", score:0}}, {text : "Happy to help.", go : "#hidecat", integrityScore: 0, relationship: {name:"cat", score:0}}], label: "choices"},
 				{type: "hide", img: cat, label: "hidecat", waitUntilHidden: false},
@@ -125,13 +125,13 @@ var MPLAY = MPLAY || {};
 		}
 		else if(this._talked == 1) {
 			o = [
-				{type: "show", img: ryan},
+				{type: "show", img: ryan, waitUntilShown: false},
 				{type: "dialog", speaker: "ryan", text: "What did I tell you, that karma!"},
 			];
 		}
 		else if(this._talked == 2) {
 			o = [
-				{type: "show", img: priya},
+				{type: "show", img: priya, waitUntilShown: false},
 				{type: "dialog", speaker: "priya", text: "What are the chances!"},
 			];
 		}
