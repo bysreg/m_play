1a44f56c0c65464985d936ec781b0724e20ac840
narumimaria <zhangyaz@andrew.cmu.edu>
2016-02-22 19:56:33 -0500

    integrated scene 1, need more adjustments

--- src/m_play/gnovel/static/gnovel/js/mplay/page0.js
+++ src/m_play/gnovel/static/gnovel/js/mplay/page0.js
@@ -27,102 +27,113 @@ var MPLAY = MPLAY || {};
 		this.setBackground("/static/gnovel/res/textures/backgrounds/enviroment concept.jpg");
 
 		//create images
-		this._professorImg = this.createImage("/static/gnovel/res/textures/char/prof sweeney- thoughtful.png", new THREE.Vector3(75, -130, 180), 600, 750);
-		this._juliImg = this.createImage("/static/gnovel/res/textures/char/thoughtful-julia.png", new THREE.Vector3(-300, -140, 120), 600, 750);
 		this._ryanImg = this.createImage("/static/gnovel/res/textures/char/ryan-happy.png", new THREE.Vector3(0, -80, 140), 600, 750);
-		this._catImg = this.createImage("/static/gnovel/res/textures/char/cat-annoyed.png", new THREE.Vector3(450, -130, 100), 600, 750);		
+		this._yourphoneImg = this.createImage("/static/gnovel/res/textures/phone.png", new THREE.Vector3(0, 60, 20), 250, 458);
+		this._closephoneImg = this.createImage("/static/gnovel/res/textures/phone.png", new THREE.Vector3(0, 60, 160), 519, 950);
+		this._catsphoneImg = this.createImage("/static/gnovel/res/textures/cat's phone.png", new THREE.Vector3(480, 0, 40), 100, 183);
 
-		this._talked = 0;
-
-		var pageObj = this;
-		var io1 = this.createInteractableObject(
-			"/static/gnovel/res/textures/Perspective-Button-Stop-icon.png", 
-			{x: -100, y: 200, width : 64, height : 64, onClick: function() {
-				pageObj._talked = 1;
-				pageObj._runFlow();
-			}});		
-
-		var io2 = this.createInteractableObject(
-			"/static/gnovel/res/textures/Perspective-Button-Stop-icon.png", 
-			{x: 100, y: 200, width : 64, height : 64, onClick: function() {
-				pageObj._talked = 2;
-				pageObj._runFlow();
-			}});		
-
-		this._professorImg.material.opacity = 0;
-		this._juliImg.material.opacity = 0;
 		this._ryanImg.material.opacity = 0;
-		this._catImg.material.opacity = 0;
+		this._yourphoneImg.material.opacity = 0;
+		this._catsphoneImg.material.opacity = 0;
+		this._closephoneImg.material.opacity = 0;
 
-		this._professor = "professor";
-		this._juli = "juli";
+
 		this._ryan = "ryan";
-		this._cat = "cat";		
+		this._yourphone = "yourphone";
+		this._catsphone = "catsphone";
+		this._closephone = "closephone";
 
 		// add object tags
-		this._setObjectTag(this._professor, this._professorImg);
-		this._setObjectTag(this._juli, this._juliImg);
 		this._setObjectTag(this._ryan, this._ryanImg);
-		this._setObjectTag(this._cat, this._catImg);
+		this._setObjectTag(this._yourphone, this._yourphoneImg);
+		this._setObjectTag(this._catsphone, this._catsphoneImg);
+		this._setObjectTag(this._closephone, this._closephoneImg);
 	};
 
 	Page0.prototype._createFlowElements = function() {
-		var professor = "%professor";
-		var juli = "%juli";
 		var ryan = "%ryan";
-		var cat = "%cat";
+		var yourphone = "%yourphone";
+		var catsphone = "%catsphone";
+		var closephone = "%closephone";
+		var player = "yourname";
 		var o = null;
 
-		if(this._talked == 1) {
-			o = [
-				{type: "show", img: ryan, waitUntilShown: false}, 
-				{type: "dialog", speaker: "ryan", text: "hi, i'm ryan"}, 				
-				
-				{type: "choices", choices : 
-					[{text: "good decision (+1 relationship)", 
-						go: "#relationshipDialogue", 
-						relationship: {name:"ryan", score:1}}, 
-					{text : "bad decision(-1 relationship)", 
-						go : "#relationshipDialogue", 
-						relationship: {name:"ryan", score:-1}}]},
+		o = [
+			// need a flow here to show a buzzing phone before choices
+			{type: "show", img: yourphone},
+			{type: "choices", 
+				choices : 
+					[{text: "Look at your Phone ", 
+						go: "#lookatphone"}, 
+					{text: "Talk to Ryan First", 
+						go: "#talktoryan"}]},
+			// need a flow here to show the phone screen before next flow, and this flow should be labeled "lookatphone"
+			{type: "hide", img: yourphone, waitUntilHiden: false, label: "lookatphone"},
+			{type: "show", img: closephone},
+			{type: "dialog", speaker: "Your phone", text: "Dear " + player + ", Glad you'll be joining us at the company.  Ryan was right - you'll make a great addition to the team.  We'll be in touch. -J. WANG"},
+			{type: "hide", img: closephone, waitUntilHiden: false},
+			{type: "show", img: ryan, position: "center", waitUntilShown: false},
+			{type: "dialog", speaker: "ryan", text: "Congratulations! I am so happy you got the job - referring you was a good call.  We can continue the magic as cubicle neighbors after graduation."},
+			{type: "choices", 
+				choices : 
+					[{text: "Yeah!  Thanks again for forwarding my resume.",
+						go: "#cheers"}, 		
+					{text : "Psyched to be working with you after graduation, Ryan!",
+						go: "#cheers"}]},
+			{type: "dialog", speaker: "ryan", text: "Cheers!  Congratulations!", label: "cheers"},
+			{type: "jump", condition: true, goTrue: "#timefade", goFalse: 1000},
 
-				{type: "dialog", speaker: "ryan", 
-					label: "relationshipDialogue",
-					relationshipThreshold: 0, 
-					text: "you have a good relationship",
-					expression: "",  
-					
-					text2: "you have a bad relationship", 
-					expression2: ""},
+			{type: "hide", img: yourphone, waitUntilHiden: false, label: "talktoryan"},
+			{type: "show", img: ryan, position: "center", waitUntilShown: false},
+			{type: "dialog", speaker: "ryan", text: "Check your phone, check your phone!"},
+			{type: "choices", 
+				choices : 
+					[{text: "What?! Just tell me!",
+						go: "#gotjob"}, 		
+					{text : "Good news?!",
+						go: "#gotjob"}]},
+			{type: "dialog", speaker: "ryan", text: "You got the job!  We’re going to be working together after graduation! You’ll love our boss.  He was so great during the internship.  I just know it’ll be great.", label: "gotjob"},
+			{type: "hide", img: ryan},
 
-				{type: "choices", 
-					choices : 
-						[{text: "good decision (+1 integrity)", 
-							go: "#integrityScore", 
-						integrityScore: 1}, 
-						
-						{text : "bad decision(-1 integrity)", 
-							go : "#integrityScore", 
-							integrityScore: -1}]},
+			// after transition
+			{type: "show", img: ryan, position: "center", waitUntilShown: false, label: "timefade"},
+			{type: "dialog", speaker: "ryan", text: "Oh man, this semester is gonna be tough.  I think our class - Programming and Society should be good though.  My brother took it last year."},
+			{type: "choices", 
+				choices : 
+					[{text: "Should be good.",
+						go: "#mentionpriya"}, 		
+					{text : "Glad we’re in it together.",
+						go: "#mentionpriya"}]},
+			{type: "dialog", speaker: "ryan", text: "I think it's cross listed CS and psych or something.  My friend Priya is in it too.  She's really nice, I’ll introduce you guys.", label: "mentionpriya"},
+			// see a phone on the table.
+			{type: "show", img: catsphone},
+			
+			{type: "dialog", speaker: "ryan", text: "Looks like someone left their phone."},
+			// this choice affects scene 2
+			{type: "choices", 
+				choices : 
+					[{text: "That sucks for them.", 
+						go: "#sucks"}, 
+					{text: "Pick up the phone to see if there’s any contact information.", 
+						go: "#pickupphone"},
+					{text: "Let’s sell it on Ebay!",
+						go: "#sellit"}]},
 
-				{type: "custom", label: "integrityScore", func: function(page) {
-					return page._integrityManager.getIntegrity();
-				}},
+			{type: "dialog", speaker: "ryan", text: "So anyway, congrats again.  Better keep up that GPA – our boss warned me before I left last summer to keep it above a 3.5.", label: "sucks"},
+			{type: "jump", condition: true, goTrue: "#hidephone", goFalse: 1000},
 
-				{type: "compare", leftop: "$integrityScore", operator: "greater equal", rightop: 0, goTrue: "#good", goFalse: "#bad"},
-				{type: "dialog", speaker: "ryan", text: "you have good ending", label: "good"}, 
-				{type: "jump", condition: true, goTrue: 1000, goFalse: 1000}, 
-				{type: "dialog", speaker: "ryan", text: "you have bad ending", label: "bad"}, 
-				{type: "goto", page: 1},
-			];
-		}		
-		else if(this._talked == 2) {
-			o = [
-				{type: "show", img: cat},
-				{type: "dialog", speaker: "cat", text: "hi, i'm cat", label : "choice_number_1"}, 
-				{type: "hide", img: cat},				
-			];
-		}
+			{type: "dialog", speaker: "ryan", text: "Oh no.  Looks like it’s dead.  Sucks to be that guy.", label: "pickupphone"},
+			{type: "choices", 
+				choices : 
+					[{text: "Let’s give it to the bartender to hold on to."}, 		
+					{text : "I’ll bring it home to charge.  Maybe the owner will contact it."}]},
+			{type: "dialog", speaker: "ryan", text: "That should score you some Karma points"},
+			{type: "jump", condition: true, goTrue: "#hidephone", goFalse: 1000},
+
+			{type: "dialog", speaker: "ryan", text: "Ha!  I didn’t know you were so mean.  Leave it, I guess.", label: "sellit"},
+			{type: "hide", img: catsphone, label: "hidephone"},
+
+		];		
 		
 		return o;
 	};
