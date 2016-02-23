9f4d5120bea15a6a8fbc8de39ff1334c22ece0fc
timmyD <timothysd@hotmail.com>
2016-02-23 13:47:37 -0500

    added removeMouseDownListener() to choices.js added new interactive object to page0.js

--- src/m_play/gnovel/static/gnovel/js/mplay/page0.js
+++ src/m_play/gnovel/static/gnovel/js/mplay/page0.js
@@ -35,6 +35,10 @@ var MPLAY = MPLAY || {};
 		this._catsphoneImg.material.opacity = 0;
 		this._closephoneImg.material.opacity = 0;
 
+		var phone = this.createInteractableObject(
+			"/static/gnovel/res/textures/phone.png",
+			{x: -100, y: 200, width : 64, height : 64, onClick: function() {
+			}});
 
 		this._ryan = "ryan";
 		this._yourphone = "yourphone";
@@ -59,11 +63,11 @@ var MPLAY = MPLAY || {};
 		o = [
 			// need a flow here to show a buzzing phone before choices
 			{type: "show", img: yourphone},
-			{type: "choices", 
-				choices : 
-					[{text: "Look at your Phone ", 
-						go: "#lookatphone"}, 
-					{text: "Talk to Ryan First", 
+			{type: "choices",
+				choices :
+					[{text: "Look at your Phone ",
+						go: "#lookatphone"},
+					{text: "Talk to Ryan First",
 						go: "#talktoryan"}]},
 			// need a flow here to show the phone screen before next flow, and this flow should be labeled "lookatphone"
 			{type: "hide", img: yourphone, waitUntilHiden: false, label: "lookatphone"},
@@ -72,10 +76,10 @@ var MPLAY = MPLAY || {};
 			{type: "hide", img: closephone, waitUntilHiden: false},
 			{type: "show", img: ryan, position: "center", waitUntilShown: false},
 			{type: "dialog", speaker: "ryan", text: "Congratulations! I am so happy you got the job - referring you was a good call.  We can continue the magic as cubicle neighbors after graduation."},
-			{type: "choices", 
-				choices : 
+			{type: "choices",
+				choices :
 					[{text: "Yeah!  Thanks again for forwarding my resume.",
-						go: "#cheers"}, 		
+						go: "#cheers"},
 					{text : "Psyched to be working with you after graduation, Ryan!",
 						go: "#cheers"}]},
 			{type: "dialog", speaker: "ryan", text: "Cheers!  Congratulations!", label: "cheers"},
@@ -84,10 +88,10 @@ var MPLAY = MPLAY || {};
 			{type: "hide", img: yourphone, waitUntilHiden: false, label: "talktoryan"},
 			{type: "show", img: ryan, position: "center", waitUntilShown: false},
 			{type: "dialog", speaker: "ryan", text: "Check your phone, check your phone!"},
-			{type: "choices", 
-				choices : 
+			{type: "choices",
+				choices :
 					[{text: "What?! Just tell me!",
-						go: "#gotjob"}, 		
+						go: "#gotjob"},
 					{text : "Good news?!",
 						go: "#gotjob"}]},
 			{type: "dialog", speaker: "ryan", text: "You got the job!  We’re going to be working together after graduation! You’ll love our boss.  He was so great during the internship.  I just know it’ll be great.", label: "gotjob"},
@@ -96,23 +100,23 @@ var MPLAY = MPLAY || {};
 			// after transition
 			{type: "show", img: ryan, position: "center", waitUntilShown: false, label: "timefade"},
 			{type: "dialog", speaker: "ryan", text: "Oh man, this semester is gonna be tough.  I think our class - Programming and Society should be good though.  My brother took it last year."},
-			{type: "choices", 
-				choices : 
+			{type: "choices",
+				choices :
 					[{text: "Should be good.",
-						go: "#mentionpriya"}, 		
+						go: "#mentionpriya"},
 					{text : "Glad we’re in it together.",
 						go: "#mentionpriya"}]},
 			{type: "dialog", speaker: "ryan", text: "I think it's cross listed CS and psych or something.  My friend Priya is in it too.  She's really nice, I’ll introduce you guys.", label: "mentionpriya"},
 			// see a phone on the table.
 			{type: "show", img: catsphone},
-			
+
 			{type: "dialog", speaker: "ryan", text: "Looks like someone left their phone."},
 			// this choice affects scene 2
-			{type: "choices", 
-				choices : 
-					[{text: "That sucks for them.", 
-						go: "#sucks"}, 
-					{text: "Pick up the phone to see if there’s any contact information.", 
+			{type: "choices",
+				choices :
+					[{text: "That sucks for them.",
+						go: "#sucks"},
+					{text: "Pick up the phone to see if there’s any contact information.",
 						go: "#pickupphone"},
 					{text: "Let’s sell it on Ebay!",
 						go: "#sellit"}]},
@@ -121,9 +125,9 @@ var MPLAY = MPLAY || {};
 			{type: "jump", condition: true, goTrue: "#hidephone", goFalse: 1000},
 
 			{type: "dialog", speaker: "ryan", text: "Oh no.  Looks like it’s dead.  Sucks to be that guy.", label: "pickupphone"},
-			{type: "choices", 
-				choices : 
-					[{text: "Let’s give it to the bartender to hold on to."}, 		
+			{type: "choices",
+				choices :
+					[{text: "Let’s give it to the bartender to hold on to."},
 					{text : "I’ll bring it home to charge.  Maybe the owner will contact it."}]},
 			{type: "dialog", speaker: "ryan", text: "That should score you some Karma points"},
 			{type: "jump", condition: true, goTrue: "#hidephone", goFalse: 1000},
@@ -131,10 +135,10 @@ var MPLAY = MPLAY || {};
 			{type: "dialog", speaker: "ryan", text: "Ha!  I didn’t know you were so mean.  Leave it, I guess.", label: "sellit"},
 			{type: "hide", img: catsphone, label: "hidephone"},
 
-		];	
+		];
 
 		return o;
 	};
-		
+
 	MPLAY.Page0 = Page0;
 }());
