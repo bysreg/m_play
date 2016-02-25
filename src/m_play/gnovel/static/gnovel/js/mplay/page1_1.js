fd32ac46608d2aca4d9e614a8aec8507e51b14d8
hilman <hilmanbeyri@gmail.com>
2016-02-25 16:57:57 -0500

    Add expression for scene 2.b

--- src/m_play/gnovel/static/gnovel/js/mplay/page1_1.js
+++ src/m_play/gnovel/static/gnovel/js/mplay/page1_1.js
@@ -60,14 +60,15 @@ var MPLAY = MPLAY || {};
 		// variables from scene 1
 		var isPhonePickedUp = (catsPhoneStatus > 0);
 		var isPhoneWithYou = (catsPhoneStatus == 2);
+		// var isPhonePickedUp = 1;
+		// var isPhoneWithYou = 0;
 		// player name which the player entered at the beginning of the game
 		var player = "Lindsey";
 		// this._talked = 1;
 
-		if(this._talked == 0) {
 			o = [
 				// transition of this flow doesn't work
-				{type: "show", img: ryan, position: "center", waitUntilShown: false},
+				{type: "show", img: ryan, expression: "happy", position: "center", waitUntilShown: false},
 				{type: "dialog", speaker: "Ryan", text: "Awesome!  Both you and Priya are in my group.  This is Priya.  She’s here from India,and an excellent study buddy."},
 				// transition of this flow doesn't work
 				{type: "hide", img: ryan, waitUntilHidden: false},
@@ -81,8 +82,11 @@ var MPLAY = MPLAY || {};
 				{type: "dialog", speaker: "Cat", text: "Hey, my name is Cat.  Uhh… I’m in the business school.  Nice to meet you guys… sorry I’m a little all over the place.  I lost my phone yesterday –"},
 				{type: "hide", img: cat, waitUntilHidden: false, flip: true},
 				{type: "compare", leftop: isPhonePickedUp, operator: "equal", rightop: 1, goTrue: "#phone_picked", goFalse: "#phone_notpicked"},
+				
+				// if you picked up the phone
 				{type: "show", img: ryan, label: "phone_picked", waitUntilShown: false},
 				{type: "compare", leftop: isPhoneWithYou, operator: "equal", rightop: 1, goTrue: "#phone_withyou", goFalse: "#phone_notwithyou"},
+				// and you left it with the bartender
 				{type: "dialog", speaker: "Ryan", text: "Were you at Scottie's Bar yesterday?  We found a phone there.", label: "phone_notwithyou"},
 				{type: "hide", img: ryan, waitUntilHidden: false},
 				{type: "show", img: cat, waitUntilShown: false, flip: true},
@@ -90,32 +94,31 @@ var MPLAY = MPLAY || {};
 				{type: "hide", img: cat, waitUntilHidden: false, flip: true},
 				{type: "show", img: ryan, waitUntilShown: false},
 				{type: "dialog", speaker: "Ryan", text: "We left it with the bartender."},
+				{type: "hide", img: ryan},
+				{type: "show", img: priya, expression: "happy", waitUntilShown: false},
+				{type: "dialog", speaker: "Priya", text: "What are the chances!"},
 				{type: "jump", condition: true, goTrue: 1000, goFalse: 1000},
+
+				// if you have the phone with you
 				{type: "dialog", speaker: "Ryan", text: "We found a phone yesterday – " + player + ", do you have it with you?", label: "phone_withyou"},
 				{type: "hide", img: ryan, waitUntilHidden: false},
-				{type: "show", img: cat, waitUntilShown: false, flip: true},
+				{type: "show", img: cat, expression: "happy",waitUntilShown: false, flip: true},
 				{type: "dialog", speaker: "Cat", text: "Oh my God, you guys have it with you?!  You both are lifesavers!"},
 				{type: "choices", choices : [{text: "No Problem.", go: "#hidecat", relationship: {name:"cat", score:0}}, {text : "Happy to help.", go : "#hidecat", integrityScore: 0, relationship: {name:"cat", score:0}}], label: "choices"},
 				{type: "hide", img: cat, label: "hidecat", waitUntilHidden: false, flip: true},
-				{type: "jump", condition: true, goTrue: 1000, goFalse: 1000},
-				{type: "show", img: ryan, label: "phone_notpicked"},
-				{type: "dialog", speaker: "Ryan", text: "Were you at Scottie's Bar yesterday?  We found a phone there"},
-				{type: "dialog", speaker: "Cat", text: "Hopefully it’s still there.  I’ll call them."},
-				{type: "hide", img: cat, waitUntilHidden: false, flip: true},
-			];
-		}
-		else if(this._talked == 1) {
-			o = [
 				{type: "show", img: ryan, waitUntilShown: false},
-				{type: "dialog", speaker: "Ryan", text: "What did I tell you, that karma!"},
+				{type: "dialog", speaker: "Ryan", text: player + " here is the lifesaver, I'm just the messenger"},
+				{type: "jump", condition: true, goTrue: 1000, goFalse: 1000},
+
+				// if you left the phone at the bar
+				{type: "show", img: ryan, label: "phone_notpicked"},
+				{type: "dialog", speaker: "Ryan", text: "I think there was a phone at Scottie's yesterday? We found a phone there."},
+				{type: "hide", img: ryan, waitUntilHidden: false},
+				{type: "show", img: cat, flip: true},
+				{type: "dialog", speaker: "Cat", text: "Hopefully it’s still there.  I’ll call them."},
+				{type: "hide", img: cat, waitUntilHidden: false},
 			];
-		}
-		else if(this._talked == 2) {
-			o = [
-				{type: "show", img: priya, waitUntilShown: false},
-				{type: "dialog", speaker: "Priya", text: "What are the chances!"},
-			];
-		}
+		
 		return o;
 	}
 
