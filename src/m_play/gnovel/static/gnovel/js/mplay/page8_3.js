4d1016111c40f6f5f4d0c426dc9789daaa38572f
hilman <hilmanbeyri@gmail.com>
2016-04-16 18:58:38 -0400

    Split page8_3.js

--- src/m_play/gnovel/static/gnovel/js/mplay/page8_3.js
+++ src/m_play/gnovel/static/gnovel/js/mplay/page8_3.js
@@ -64,50 +64,8 @@ var MPLAY = MPLAY || {};
 			{type: "hide", img: this._professor},
 
 			{type: "nothing", label: "email"},
-			{type: "show_phone_notif"},
-
-			// phone email exchange begins
-			{type: "open_phone", layout:"email", subject: "Academic Violation", from: "Prof. Sweeney", email: "sweeney@andrew.cmu.edu",
-				text: "After much deliberation, I have decided to address your violation of the University’s Policy on Academic Integrity with the following action: As a consequence of unauthorized possession and use of last year’s exam, you will need to retake the take home exam, which will be different from the exam previously provided this semester. As we discussed in our meeting, I will cut you some slack though I am extremely disappointed.  I will be formally advancing this violation to the Division of Student Affairs for additional follow up.  Know that if I am made aware of an additional violation in this course, I will be reporting that as well.  There is no statute of limitations in reporting a violation.  Please take this as a learning experience to reflect on your actions.  -Prof. Sweeney"},
-			{type: "close_phone"},
-
-			{type: "show_context", text: "You receive a text from Ryan"},
-
-
-			{type: "custom", func: function(page){
-				return page.getRelationshipManager().getRelationship("Ryan");
-			}, label: "ryanRelationshipScore1"},
-			{type: "compare", leftop: "$ryanRelationshipScore1", operator: "greater", rightop: 0, goTrue: "#pos-ryan1", goFalse: "#compareryan1"},
-
-			{type: "nothing", label: "pos-ryan1"},
-			// phone exchange begins
-			{type: "open_phone", layout:"text", people: [this._ryan]},
-			{type: "add_phone_textbox",
-				speaker: this._ryan,
-				text: player + ", did you get Sweeney’s email?  What now? If we fail the class, don’t know about the job… this wasn’t worth it."},
-			{type: "close_phone"},
-			{type: "goto", page: "scene 10.a"},
-
-			{type: "nothing", label: "compareryan1"},
-			{type: "compare", leftop: "$ryanRelationshipScore1", operator: "equal", rightop: 0, goTrue: "#zero-ryan1", goFalse: "#neg-ryan1"},			
-
-			{type: "nothing", label: "zero-ryan1"},
-			// phone exchange begins
-			{type: "open_phone", layout:"text", people: [this._ryan]},
-			{type: "add_phone_textbox",
-				speaker: this._ryan,
-				text: "You get Sweeney’s email?  What now? Do you think the job thing is ok?  This wasn’t worth it…"},
-			{type: "close_phone"},
-			{type: "goto", page: "scene 10.a"},
-
-			{type: "nothing", label: "neg-ryan1"},
-			// phone exchange begins
-			{type: "open_phone", layout:"text", people: [this._ryan]},
-			{type: "add_phone_textbox",
-				speaker: this._ryan,
-				text: "Did you get Sweeney’s email?  What now?  This wasn’t worth it…"},
-			{type: "close_phone"},
-			{type: "goto", page: "scene 10.a"},
+			
+			{type: "goto", page: "scene 9.d.a"},
 		];
 
 		return o;
