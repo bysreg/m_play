3280c4b447f727fcfb410dfa162ce26fb6b84f89
hilman <hilmanbeyri@gmail.com>
2016-04-16 20:21:34 -0400

    set to player's name in page5_1.js

--- src/m_play/gnovel/static/gnovel/js/mplay/page5_1.js
+++ src/m_play/gnovel/static/gnovel/js/mplay/page5_1.js
@@ -239,7 +239,7 @@ var MPLAY = MPLAY || {};
 			{type: "open_phone", layout:"text", people: [this._ryan]},
 			{type: "add_phone_textbox",
 				speaker: this._ryan,
-				text: "Hey [You] – do you have time to meet up today?"},
+				text: "Hey "+ player +" – do you have time to meet up today?"},
 			{type: "choices", choices : [
 				{text: "I’m heading to the gym, do u want to join me?",
 					go: "#gogym"},
